4c41421a4ae196459c98e427b8baafe8394c1994
Awan Cloud <aoneholics@gmail.com>
2020-09-15 12:01:42 +0700

    update sept 2020

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EcommerceWebsite';
+
+  loadAPI: Promise<any>;
+
+  constructor() {        
+      this.loadAPI = new Promise((resolve) => {
+          this.loadScript();
+          resolve(true);
+      });
+  }
+
+  public loadScript() {        
+      var isFound = false;
+      var scripts = document.getElementsByTagName("script")
+      for (var i = 0; i < scripts.length; ++i) {
+          if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes("loader")) {
+              isFound = true;
+          }
+      }
+
+      if (!isFound) {
+          var dynamicScripts = [
+            "../assets/js/vendor/modernizr-2.8.3.min.js",
+            "../assets/js/vendor/jquery.min.js",
+            "../assets/js/popper.min.js",
+            "../assets/js/bootstrap.min.js",
+            "../assets/js/plugins.js",
+            "../assets/js/main.js"
+          ];
+
+          for (var i = 0; i < dynamicScripts.length; i++) {
+              let node = document.createElement('script');
+              node.src = dynamicScripts [i];
+              node.type = 'text/javascript';
+              node.async = false;
+              node.charset = 'utf-8';
+              document.getElementsByTagName('head')[0].appendChild(node);
+          }
+
+      }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+import { TransactionComponent } from './transaction/transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    CartComponent,
+    CheckoutComponent,
+    ProfileComponent,
+    RegisterComponent,
+    TransactionComponent
   ],
   imports: [
     BrowserModule,
